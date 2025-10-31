7862ed255607f9ebc93524cc1b80e75ab724ffa9
SergiNaranjo <sergi.naranjo@enti.cat>
2025-10-31 10:49:00 +0100

    Some changes

--- SDL/source/ImageRenderer.h
+++ SDL/source/ImageRenderer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Renderer.h"
+#include "RenderManager.h"
 
 class ImageRenderer : public Renderer
 {
@@ -8,4 +9,25 @@ public:
 	void LoadTexture(std::string texturePath, SDL_Renderer* renderer) override;
 	void Update();
 	void Render(SDL_Renderer* renderer);
+
+	ImageRenderer(Transform* transform, std::string resourcePath, Vector2 sourceOffset, Vector2 sourceSize) : Renderer(transform, resourcePath)
+	{
+		RM->GetTexture(resourcePath);
+
+		_sourceRect = SDL_FRect
+		{
+			sourceOffset.x,
+			sourceOffset.y,
+			sourceSize.x,
+			sourceSize.y
+		};
+
+		_destinationRect = SDL_FRect
+		{
+			transform->position.x,
+			transform->position.y,
+			transform->scale.x,
+			transform->scale.y
+		};
+	}
 };
--- SDL/source/Vector2.h
+++ SDL/source/Vector2.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <math.h>
+
 class Vector2
 {
 public:
@@ -7,4 +9,36 @@ public:
 	float y;
 	Vector2() : x(0.0f), y(0.0f) {}
 	Vector2(float _x, float _y) : x(_x), y(_y) {}
+
+	void Normalize()
+	{
+		float legth = sqrt(x * x + y * y);
+		x /= legth;
+		y /= legth;
+	}
+
+	Vector2 const operator+(const Vector2& other) const
+	{
+		return Vector2(x + other.x, y + other.y);
+	}
+
+	Vector2 const operator-(const Vector2& other) const
+	{
+		return Vector2(x - other.x, y - other.y);
+	}
+
+	Vector2 const operator*(const float& other) const
+	{
+		return Vector2(x * other, y * other);
+	}
+
+	Vector2 const operator*(const Vector2& other) const
+	{
+		return Vector2(x * other.x, y * other.y);
+	}
+
+	Vector2 const operator/(const float& other) const
+	{
+		return Vector2(x / other, y / other);
+	}
 };
